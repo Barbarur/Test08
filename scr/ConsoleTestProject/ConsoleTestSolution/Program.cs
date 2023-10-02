bb144892304fc67585b1583bca133cbd537d40fd
Barbarur <alvaro.avila@protonmail.com>
2023-10-02 21:13:19 +0800

    First code commit

---
+++ scr/ConsoleTestProject/ConsoleTestSolution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleTestSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
